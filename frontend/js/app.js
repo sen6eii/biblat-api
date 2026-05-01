c1c2761fb430dfb665c7ced31efc17ba158ec2d6
Daniel <dgmartinez.uy@gmail.com>
2026-05-01 20:43:51 -0300

    Fix: API_BASE dinamico para produccion

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3000';
+const API_BASE = window.location.hostname === 'localhost' ? 'http://localhost:3000' : '';
 
 let state = {
   apiKey: localStorage.getItem('biblat_api_key') || '',
